55971b2c9f6050b15043408d242fa292d8f7f55c
mingik <mingiyan@gmail.com>
2016-07-30 13:57:59 -0700

    renamed folder

--- strings/anagrams.scala
+++ strings/anagrams.scala
@@ -2,17 +2,14 @@
 
 Generate ALL anagrams of a Given String
 
-  */
+Idea: for each current character in a string, recurse on remaining
+substring and then 'inject' this character in all available positions
+before/after and between.
 
-def anagrams(s: List[Char]): List[List[Char]] = {
-  s.toList match {
-    case Nil => List(List())
-    case a :: rest => {
-      val recurse = anagrams(rest)
-      recurse.flatMap( li => inject(a, li) )
-    }
-  }
-}
+Usage: scala> anagrams( "hello".toList ).map( li => li.foldLeft("")((s,ch) =>
+s+ch) )
+
+*/
 
 /**
 'Injects' given character into given list into all possible places
@@ -23,6 +20,16 @@ def inject(a: Char, li: List[Char]): List[List[Char]] = {
     case b::rest => {
       val recurse: List[List[Char]] = inject(a,rest)
       List(a::(b::rest)) ++ recurse.map(li => b::li)
+    }
+  }
+}
+
+def anagrams(s: List[Char]): List[List[Char]] = {
+  s.toList match {
+    case Nil => List(List())
+    case a :: rest => {
+      val recurse = anagrams(rest)
+      recurse.flatMap( li => inject(a, li) )
     }
   }
 }
